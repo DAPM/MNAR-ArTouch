23d1db14015a3bd43e6e3f212c3bb7a9fef2e447
iemilia <ilincai.emilia@gmail.com>
2013-11-18 14:57:30 +0200

    Issue #4 - fixed problems

---
+++ ArTouch/src/com/example/artouch/TapCounter.java
@@ -0,0 +1,24 @@
+package com.example.mnar_task4;
+
+import android.os.CountDownTimer;
+
+public class TapCounter extends CountDownTimer  {
+	
+	public TapCounter(long millisInFuture, long countDownInterval) {
+         super(millisInFuture, countDownInterval);
+     }
+
+     @Override
+     public void onFinish() {
+     }
+
+     @Override
+     public void onTick(long millisUntilFinished) {
+     }
+
+    
+	public void resetCounter() {
+		// TODO Auto-generated method stub
+		start();
+	} 
+}
