58d337fd1d332c9944ce6dddcf4a5b44d795f5b9
iemilia <ilincai.emilia@gmail.com>
2013-11-18 14:36:29 +0200

    Issue #4

--- ArTouch/gen/com/example/mnar_task4/BuildConfig.java
+++ ArTouch/gen/com/example/mnar_task4/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.example.artouch;
+package com.example.mnar_task4;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
