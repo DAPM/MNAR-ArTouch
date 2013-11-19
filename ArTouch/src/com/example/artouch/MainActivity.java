7b71af4bf912883a71a2579c9bbd44f1f13d79a7
RBalan <balan_razvan_alin@yahoo.com>
2013-11-19 22:29:00 +0200

    Issue #12 - Create a first selection screen
    Created a screen for testing purposes in which you can choose the screen
    you want to see and the language.
    
    Implemented the change of language and access to the appropriate
    resource depending on the language chosen. Resources are found in:
    /res/values; /res/values-fr; /res/values-ro.

--- ArTouch/src/com/example/artouch/MainActivity.java
+++ ArTouch/src/com/example/artouch/MainActivity.java
@@ -1,110 +1,111 @@
 package com.example.artouch;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
-import com.example.artouch.R;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MotionEvent;
-import android.widget.TextView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
-	
-	//variabile ptr succedare limbi
-	public final int TAP_LIMIT = 3;
-	public final String LANG_CHOICE[] = {"Apasati de " + TAP_LIMIT + " ori pe ecran pentru a selecta limba Romana","Tap " + TAP_LIMIT + " times to choose English language","Tappez"};
-	public final String LANG_PICKED[] = {"Ati ales limba ROMANA","You have chosen ENGLISH","Vous avez choisi FRANCAIS"};
-	public boolean tapLimitReached = false;
-	
-	public Timer timer = new Timer(); //TAP_LIMIT second timer
-	public TextView languageTextView; 
-		
-	public int langSelectat=-1;
-	/*
-	0=for romanian
-  	1=for english	 
- 	2=for french	
-	*/
-	
-	 //variabile counter tap
-	 protected static final long TAP_MAX_DELAY = 2000;
-	 private TapCounter tapCounter = new TapCounter(TAP_MAX_DELAY, TAP_MAX_DELAY);
-	 private int lastTapCount = 0;
-	 
-	 	
-	@Override	
-	protected void onCreate(Bundle savedInstanceState) 
-	{
+
+	private Spinner languageSpinner, screenSpinner;
+	private Button btnSubmit;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	    languageTextView = (TextView)findViewById(R.id.textLimba);	    
 		
-		timer.scheduleAtFixedRate(new TimerTask() 
-		{
-		    @Override
-		    public void run() 
-		    {
-		        //Called each time when 1000 milliseconds (1 second) (the period parameter)
-		    	if(tapLimitReached == false)
-		    	{
-		    		if(langSelectat < TAP_LIMIT - 1) 
-		    			langSelectat++;
-
-		    		//nu intra niciodata pe aici?
-                    else 
-                    	langSelectat = 0;
-		    		
-		    		runOnUiThread(new Runnable() 
-		    		{
-		    			@Override
-		    			public void run() 
-		    			{
-		    				lastTapCount = 0;
-		    				languageTextView.setText(LANG_CHOICE[langSelectat] + lastTapCount);
-		    			}
-		    		});
-		    	}		    	
-		    }
-
-		},
-		//Set how long before to start calling the TimerTask (in milliseconds)
-		1000,
-		//Set the amount of time between each execution (in milliseconds)
-		6000);
-		//TimerTask schimbaLimba=new SchimbaLimba(MainActivity.this); ii zic ce actiune trebuie sa faca in fiecare interval
-       // t.scheduleAtFixedRate(schimbaLimba, 0, 3000); il programez sa inceapa
-	}	 			 
-		
-	public boolean onTouchEvent(MotionEvent event)
-	{		    
-		   if(event.getAction() == MotionEvent.ACTION_UP)
-		   { 
-			   lastTapCount++;
-			   tapCounter.resetCounter(); //reset counter between taps
-			   if (lastTapCount == TAP_LIMIT) 
-			   {
-				   languageTextView.setText(LANG_PICKED[langSelectat] + lastTapCount);
-				   timer.cancel();
-				   
-				   tapLimitReached = true;
-				   
-				   Intent secondScreen = new Intent(MainActivity.this, SecondScreen.class);
-				   startActivity(secondScreen);
-			   } 
-		   }
-		  
-		   return true;
+		addItemsOnSpinners();	
+		addListenerOnButton();
 	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) 
+ 
+	/** 
+	 * add items into spinners dynamically
+	 * @author Razvan
+	 */
+	public void addItemsOnSpinners() 
 	{
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		screenSpinner = (Spinner) findViewById(R.id.screenSelection);
+		List<String> list = new ArrayList<String>();
+		list.add("Screen 1");
+		list.add("Screen 2");
+		list.add("Screen 3");
+		
+		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_item, list);
+		dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		screenSpinner.setAdapter(dataAdapter);
+
+		languageSpinner = (Spinner) findViewById(R.id.languageSelection);
+		List<String> list2 = new ArrayList<String>();
+		list2.add("Romana");
+		list2.add("English");
+		list2.add("Francais");       
+		
+		ArrayAdapter<String> dataAdapter2 = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_item, list2);
+		dataAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		languageSpinner.setAdapter(dataAdapter2);
+	}
+
+	// get the selected dropdown list value
+	public void addListenerOnButton() 
+	{
+		languageSpinner = (Spinner) findViewById(R.id.languageSelection);
+		screenSpinner = (Spinner) findViewById(R.id.screenSelection);
+		btnSubmit = (Button) findViewById(R.id.startButton);
+		btnSubmit.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				String languageChosen = languageSpinner.getSelectedItem().toString();
+				String screenChosen = screenSpinner.getSelectedItem().toString();
+
+				Configuration c = new Configuration(getResources().getConfiguration());
+
+				if(languageChosen.equals("English")) 
+				{
+					c.locale = Locale.ENGLISH;
+				}
+				else if(languageChosen.equals("Francais")) 
+				{
+					c.locale = Locale.FRENCH;
+				}
+				else if(languageChosen.equals("Romana")) 
+				{
+					c.locale = new Locale("ro", "RO");
+				}
+				getResources().updateConfiguration(c, getResources().getDisplayMetrics());	
+
+				Intent intent;
+				if(screenChosen.equals("Screen 1"))
+				{
+					intent = new Intent(MainActivity.this, SelectLanguageActivity.class);
+					startActivity(intent);
+				}
+				else if(screenChosen.equals("Screen 2"))
+				{
+					intent = new Intent(MainActivity.this, TutorialActivity.class);
+					startActivity(intent);
+				}
+				else if(screenChosen.equals("Screen 3"))
+				{
+					intent = new Intent(MainActivity.this, PaintingExplorerActivity.class);
+					startActivity(intent);
+				}
+
+			}
+
+		});
 	}
 }
+
