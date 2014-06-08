35f56ba49534036cd818ac13fefbd8fc6d1fc690
Razvan Balan <razvan.balan@cst-us.com>
2014-06-08 20:31:54 +0300

    Signac: Reset uioptions after each system ui visibility change to make system bars disappear

--- Signac/src/ro/upb/ing/signac/MainActivity.java
+++ Signac/src/ro/upb/ing/signac/MainActivity.java
@@ -63,6 +63,17 @@ public class MainActivity extends Activity {
 	                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
 			decorView.setSystemUiVisibility(uiOptions);
 
+			getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+    			@Override
+			    public void onSystemUiVisibilityChange(int visibility) {
+    	    		if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+    	    			int uiOptions =  View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+	               				 | View.SYSTEM_UI_FLAG_FULLSCREEN
+	              			     | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+						getWindow().getDecorView().setSystemUiVisibility(uiOptions);
+					}
+		    	}
+			});
 		}		
 		setContentView(R.layout.activity_main);
 		//Let the show begin !!!
